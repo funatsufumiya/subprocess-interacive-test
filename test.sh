4e5cdbb57d3c6c0d783954a41af6406aa5de3ddc
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2023-11-13 14:38:30 +0900

    tiny update

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo run -- expect -c 'spawn -noecho scp anno-server:~/Downloads/100M.bin 100M.bin; expect eof'
+cargo run -- expect -c 'spawn -noecho scp my-server:~/Downloads/100M.bin 100M.bin; expect eof'
